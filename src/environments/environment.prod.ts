e7ee1dff8cfb60a23871d62380b97c6351096534
yaswanthsai009 <yaswanthsai.43@gmail.com>
2022-08-08 08:53:58 +0100

    changes1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCYVMlHlrkNsMojArhLgXp-3hLCZaaJIrI",
+    authDomain: "project01-356805.firebaseapp.com",
+    projectId: "project01-356805",
+    storageBucket: "project01-356805.appspot.com",
+    messagingSenderId: "667496490294",
+    appId: "1:667496490294:web:ca5d4332c84dfe69e17f97",
+    measurementId: "G-DCR7YY2WPP"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyCYVMlHlrkNsMojArhLgXp-3hLCZaaJIrI",
+    authDomain: "project01-356805.firebaseapp.com",
+    projectId: "project01-356805",
+    storageBucket: "project01-356805.appspot.com",
+    messagingSenderId: "667496490294",
+    appId: "1:667496490294:web:ca5d4332c84dfe69e17f97",
+    measurementId: "G-DCR7YY2WPP"
+  }
 };
 
 /*
